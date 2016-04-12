0f5b5b0ecad4b282543135a6f61830478f46c8b7
matteo <matteo@themostaza.com>
2016-04-12 15:41:46 +0200

    Added promises to index.android.js

--- index.android.js
+++ index.android.js
@@ -2,21 +2,40 @@ import { NativeModules } from 'react-native'
 
 const beaconsAndroid = NativeModules.BeaconsAndroidModule
 
-export const parsers = {
-  PARSER_IBEACON = 'm:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24'
+export const PARSER_IBEACON = 'm:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24'
+
+const tramissionSupport = {
+  0: 'SUPPORTED',
+  1: 'NOT_SUPPORTED_MIN_SDK',
+  2: 'NOT_SUPPORTED_BLE',
+  3: 'DEPRECATED_NOT_SUPPORTED_MULTIPLE_ADVERTISEMENTS',
+  4: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER',
+  5: 'NOT_SUPPORTED_CANNOT_GET_ADVERTISER_MULTIPLE_ADVERTISEMENTS'
 }
 
-const checkTransmissionSupported = () =>
-  beaconsAndroid.checkTransmissionSupported()
+const addParser = beaconsAndroid.addParser
 
-const startMonitoring = (regionId, beaconsUUID) =>
-  beaconsAndroid.startMonitoring(regionId, beaconsUUID)
+const checkTransmissionSupported = () => new Promise((resolve, reject) => {
+  beaconsAndroid.checkTransmissionSupported(status => resolve(tramissionSupport[status]))
+})
 
-const startRanging = (regionId, beaconsUUID) =>
-  beaconsAndroid.startRanging(regionId, beaconsUUID)
+const startMonitoring = (regionId, beaconsUUID) => new Promise((resolve, reject) => {
+  beaconsAndroid.startMonitoring(regionId, beaconsUUID, resolve, reject)
+})
+
+const startRanging = (regionId, beaconsUUID) => new Promise((resolve, reject) => {
+  beaconsAndroid.startRanging(regionId, beaconsUUID, resolve, reject)
+})
+
+const stopMonitoring = () => new Promise(beaconsAndroid.stopMonitoring)
+
+const stopRanging = () => new Promise(beaconsAndroid.stopRanging)
 
 export default {
+  addParser,
   checkTransmissionSupported,
   startMonitoring,
-
+  startRanging,
+  stopMonitoring,
+  stopRanging
 }
