586049de32776db49e6cabcd4a16d2f6c5adc84b
mmazzarolo <mazzarolomatteo@gmail.com>
2016-04-22 15:35:09 +0200

    Major and Minor as int

--- android/src/main/java/com/mmazzarolo/beaconsandroid/BeaconsAndroidModule.java
+++ android/src/main/java/com/mmazzarolo/beaconsandroid/BeaconsAndroidModule.java
@@ -234,8 +234,8 @@ public class BeaconsAndroidModule extends ReactContextBaseJavaModule {
         for (Beacon beacon : beacons) {
             WritableMap b = new WritableNativeMap();
             b.putString("uuid", beacon.getId1().toString());
-            b.putString("minor", beacon.getId2().toString());
-            b.putString("major", beacon.getId3().toString());
+            b.putInt("minor", beacon.getId2().toInt());
+            b.putInt("major", beacon.getId3().toInt());
             b.putInt("rssi", beacon.getRssi());
             b.putDouble("distance", beacon.getDistance());
             a.pushMap(b);
