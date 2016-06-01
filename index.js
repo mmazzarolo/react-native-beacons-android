3fd2b8a24a6a3e1b46d31d6cd1fd8971a5402600
mmazzarolo <mazzarolomatteo@gmail.com>
2016-06-01 09:25:39 +0200

    Fixed compatibility issue with require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { NativeModules } from 'react-native'
+const ReactNative = require('react-native')
 
-const beaconsAndroid = NativeModules.BeaconsAndroidModule
+const beaconsAndroid = ReactNative.NativeModules.BeaconsAndroidModule
 
 const PARSER_IBEACON = 'm:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24'
 const PARSER_ESTIMOTE = 'm:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24'
@@ -58,7 +58,7 @@ const stopRangingBeaconsInRegion = () => {
   return new Promise(beaconsAndroid.stopRanging)
 }
 
-export default {
+module.exports = {
   detectIBeacons,
   detectEstimotes,
   detectCustomBeaconLayout,
